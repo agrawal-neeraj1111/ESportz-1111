85fcc37048aa54e3535f457282dba03fd8d34571
agrawal-neeraj11 <deepak9782019571@gmail.com>
2023-06-21 09:30:53 +0530

    bdh

--- loader.js
+++ loader.js
@@ -5,3 +5,20 @@ loader.classList.remove('hidden');
 setTimeout(function () {
     loader.classList.add('hidden');
 }, 1500);
+function changeText() {
+    var box = document.getElementById("box");
+    
+    if (!box.classList.contains("hovered")) {
+      box.classList.add("hovered");
+      box.textContent = "To my World";
+    }
+  }
+  
+  function resetText() {
+    var box = document.getElementById("box");
+    
+    if (box.classList.contains("hovered")) {
+      box.classList.remove("hovered");
+      box.textContent = "Welcome";
+    }
+  }
